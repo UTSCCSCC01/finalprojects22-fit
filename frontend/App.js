ef7e016ded8b7e0c76cbf404d8bb6e68b6a71aa0
MatthewMelchior <61060189+MatthewMelchior@users.noreply.github.com>
2022-06-05 21:36:16 -0400

    Add buttons & search bar
    Add button for each muscle group which moves user to a different page using props to keep track of what the user pressed. Add search bar that creates event when user finishes editing.

--- frontend/App.js
+++ frontend/App.js
@@ -2,27 +2,85 @@ import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 function HomeScreen({ navigation }) {
+  const [onChangeText] = React.useState(null);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onSubmitEditing ={(event) => (Alert.alert(event.nativeEvent.text))}
+          placeholder="Search for exercises"
+        />
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        title="Abs"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Abs',
+        })}
+      />
+      <Button
+        title="Back"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Back',
+        })}
+      />
+      <Button
+        title="Biceps"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Biceps',
+        })}
+      />
+      <Button
+        title="Calves"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Calves',
+        })}
+      />
+      <Button
+        title="Cardio"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Cardio',
+        })}
+      />
+      <Button
+        title="Chest"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Chest',
+        })}
+      />
+      <Button
+        title="Legs"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Legs',
+        })}
+      />
+      <Button
+        title="Shoulders"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Shoulders',
+        })}
+      />
+      <Button
+        title="Triceps"
+        onPress={() => navigation.navigate('Details', {
+          exerciseType: 'Triceps',
+        })}
       />
     </View>
   );
 }
 
-function DetailsScreen({ navigation }) {
+function DetailsScreen({ route, navigation }) {
+  const { exerciseType } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>exerciseType: {JSON.stringify(exerciseType)}</Text>
       <Button
         title="Go to Home Screen"
-        onPress={() => navigation.navigate('Home')}
+        onPress={() => navigation.goBack()}
       />
     </View>
   );
@@ -34,7 +92,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Select Exercise" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
   </NavigationContainer>
@@ -44,9 +102,18 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 16,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 

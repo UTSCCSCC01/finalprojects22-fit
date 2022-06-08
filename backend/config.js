a4378d00d11ad7f7900517e76bfa85b4d153e872
ZyeG <joy.gong@mail.utoronto.ca>
2022-06-08 11:09:23 -0400

    merging main and FIT-8

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+let config = {
+    dbUrl:
+      "mongodb+srv://<username>:<password>@cluster0.8dilqlw.mongodb.net/<database-name>?retryWrites=true&w=majority",
+};
+  
+module.exports = config;
+  
---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// specify port number here
+const port = 3000;
+const config = require("./config");
+// import defined users route
+const usersRouter = require("./routes/users");
+
+app.use(logger("dev"));
+
+const dbUrl = config.dbUrl;
+
+var options = {
+  keepAlive: true,
+  connectTimeoutMS: 35000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// connect to database
+mongoose.connect(dbUrl, options, (err) => {
+  if (err) console.log(err);
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/users", usersRouter); // define request route
+
+app.listen(port, function () {
+  console.log("Runnning on " + port);
+});
+
+module.exports = app;

2716382042d56f0d62a78839e38b8decfc44e351
ZyeG <joy.gong@mail.utoronto.ca>
2022-06-08 10:54:08 -0400

    merging FIT-8 with main

--- frontend/App.js
+++ frontend/App.js
@@ -2,14 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +14,60 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+export default function App() {
+  
+   /* Resets the game back to default */
+   const restartSurvey = () => {
+    setCurrentQuestion(0);
+    setShowResult(false);
+    setResults([]);
+  };
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showResult, setShowResult] = useState(false);
+  const [results, setResults] = useState([]);
+  
+	const handleAnswerOptionClick = (answerText) => {
+		setResults(oldArray => [...oldArray, answerText]);
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowResult(true);
+		}
+	};
+  
+	return (
+    
+		<div className='app'>
+      <h1>FIT</h1>
+      <h2>Survey</h2>
+			{showResult ? (
+        
+				<div className='result-section'>
+					<h2>Your Input: {results}</h2>
+          {/* <h2>Your Plan: {results_plan}</h2> */}
+          <h2><button onClick={() => restartSurvey()}>Restart Survey</button></h2>
+				</div>
+        
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.answerText)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}

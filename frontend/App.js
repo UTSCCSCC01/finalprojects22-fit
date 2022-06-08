ce7e0c3a4c4e1faf8823d645952a7cd2251d43fc
ZyeG <joy.gong@mail.utoronto.ca>
2022-06-08 11:27:01 -0400

    merging main and FIT-8

--- frontend/App.js
+++ frontend/App.js
@@ -1,24 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
+import React, { useState } from 'react';
+import './App.css';
 
 export default function App() {
-  
+	const questions = [
+		{
+			questionText: 'What is your primary fitness goal?',
+			answerOptions: [
+				{ answerText: 'Gain weight/build muscle.'},
+				{ answerText: 'Lose weight/burn fat.'},
+				{ answerText: 'Improve Strength.'},
+				{ answerText: 'Improve Cardio.'},
+        { answerText: 'General Health.'},
+			],
+		},
+		{
+			questionText: 'Will you have access to gym equipment?',
+			answerOptions: [
+				{ answerText: 'Yes.'},
+				{ answerText: 'No.'},
+			],
+		},
+		{
+			questionText: 'How many days per week would you prefer to work-out?',
+			answerOptions: [
+				{ answerText: '<= 3 days/week.'},
+				{ answerText: '>4 days/week'},
+			],
+		},
+	];
+
    /* Resets the game back to default */
    const restartSurvey = () => {
     setCurrentQuestion(0);
@@ -26,10 +36,35 @@ export default function App() {
     setResults([]);
   };
 
+
+  global.results_plan = {
+    'Gain weight/build muscle.Yes.<= 3 days/week.': 'plan1',
+    'Lose weight/burn fat.Yes.<= 3 days/week.': 'plan2',
+    'Improve Strength.Yes.<= 3 days/week.': 'plan3',
+    'Improve Cardio.Yes.<= 3 days/week.' : 'plan4',
+    'General Health.Yes.<= 3 days/week.' : 'plan5',
+    'Gain weight/build muscle.No.<= 3 days/week.' : 'plan6',
+    'Lose weight/burn fat.No.<= 3 days/week.' : 'plan7',
+    'Improve Strength.No.<= 3 days/week.' : 'plan8',
+    'Improve Cardio.No.<= 3 days/week.' : 'plan9',
+    'General Health.No.<= 3 days/week.' : 'plan10',
+    'Gain weight/build muscle.Yes.>4 days/week.' : 'plan11',
+    'Lose weight/burn fat.Yes.>4 days/week.' : 'plan12',
+    'Improve Strength.Yes.>4 days/week.' : 'plan13',
+    'Improve Cardio.Yes.>4 days/week.' : 'plan14',
+    'General Health.Yes.>4 days/week.' : 'plan15',
+    'Gain weight/build muscle.No.>4 days/week.' : 'plan16',
+    'Lose weight/burn fat.No.>4 days/week.' : 'plan17',
+    'Improve Strength.No.>4 days/week.' : 'plan18',
+    'Improve Cardio.No.>4 days/week.' : 'plan19',
+    'General Health.No.>4 days/week.' : 'plan20'
+  }
+
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showResult, setShowResult] = useState(false);
   const [results, setResults] = useState([]);
-  
+  //const[plan, setPlan] = useState([]);
+
 	const handleAnswerOptionClick = (answerText) => {
 		setResults(oldArray => [...oldArray, answerText]);
 		const nextQuestion = currentQuestion + 1;
@@ -39,17 +74,13 @@ export default function App() {
 			setShowResult(true);
 		}
 	};
-  
 	return (
-    
 		<div className='app'>
       <h1>FIT</h1>
       <h2>Survey</h2>
 			{showResult ? (
-        
 				<div className='result-section'>
-					<h2>Your Input: {results}</h2>
-          {/* <h2>Your Plan: {results_plan}</h2> */}
+					<h1>your Input: {results}</h1>
           <h2><button onClick={() => restartSurvey()}>Restart Survey</button></h2>
 				</div>
         

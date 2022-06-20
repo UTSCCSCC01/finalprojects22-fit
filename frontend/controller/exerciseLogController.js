e33c37d467aa462c09852e5f32ca83f9953e9381
MatthewMelchior <61060189+MatthewMelchior@users.noreply.github.com>
2022-06-20 16:11:50 -0400

    (FIT-51) update exercise log controller

--- frontend/controller/exerciseLogController.js
+++ frontend/controller/exerciseLogController.js
@@ -1,14 +1,47 @@
 import { retrieveUserId } from '../utility/dataHandler.js'
 
-export const getExerciseSets = async () => {
+export const getExerciseSets = async (date) => {
   const userId = await retrieveUserId();
-  const response = await fetch('http://localhost:3000/set/'.concat(userId));
+  const response = await fetch('http://localhost:3000/set/'.concat(userId).concat("/").concat(date));
+  const json = await response.json();
+  return json;
+}
+
+export const getUserActivity = async (date) => {
+  const userId = await retrieveUserId();
+  const response = await fetch('http://localhost:3000/userActivity/'.concat(userId).concat("/").concat(date))
   const json = await response.json();
   return json;
 }
 
 export const deleteExerciseSet = async (id) => {
   const response = await fetch('http://localhost:3000/set/'.concat(id), {method: 'DELETE'})
+  const json = await response.json();
+  return json;
+}
+
+export const postUserActivity = async (body) => {
+  const response = await fetch('http://localhost:3000/userActivity/', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: body
+  });
+  const json = await response.json();
+  return json;
+}
+
+export const patchUserActivity = async (id, body) => {
+  const response = await fetch('http://localhost:3000/userActivity/'.concat(id), {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: body
+  });
   const json = await response.json();
   return json;
 }

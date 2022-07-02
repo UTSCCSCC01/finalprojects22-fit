27cc4d41151347bebc52823c6868bf666fd39798
Collin Chan <chancollin5101@gmail.com>
2022-07-02 04:12:41 -0400

    fixed body_metric controllers

--- frontend/controller/Body Metric/bodyMetricRecorderController.js
+++ frontend/controller/Body Metric/bodyMetricRecorderController.js
@@ -1,7 +1,20 @@
 import { retrieveUserId } from '../utility/dataHandler.js'
   
-export const patchBMetric = async (id, body) => {
-    const response = await fetch('http://localhost:3000/users/'.concat(id), {
+export const patchAddBMetric = async (userId, body) => {
+    const response = await fetch('http://localhost:3000/users/'.concat(userId)+'bmetric', {
+        method: 'PATCH',
+        headers: {
+        Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: body
+    });
+    const json = await response.json();
+    return json;
+}
+
+export const patchUpdateBMetric = async (userId, body, bid) => {
+    const response = await fetch('http://localhost:3000/users/'.concat(userId)+'bmetric'.concat(bid), {
         method: 'PATCH',
         headers: {
         Accept: 'application/json',

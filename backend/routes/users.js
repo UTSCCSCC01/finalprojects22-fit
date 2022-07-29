5505debb81a44ceafbc640f07645c05588800dab
Collin Chan <chancollin5101@gmail.com>
2022-07-29 08:59:52 -0400

    added GET req for searching on users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -62,6 +62,36 @@ router.get("/:userId", async (req, res) => {
     } 
 });
 
+// GET - Fetch users based on a string that is in user's display name/ first name/ last name/ username from the Users Collection
+router.get("/search/:query", async (req, res) => {
+    try {
+        let user = await User.find({
+            "$or" :[
+                {"display_name": new RegExp(req.params.query, 'i')},
+                {"username": new RegExp(req.params.query, 'i')}
+            ]}
+        );
+        if (user) {
+            // user is found
+            res.status(200).json({
+                status: 200,
+                data: user,
+            });
+        } else {
+            // user cannot be found in db
+            res.status(404).json({
+                status: 404,
+                message: "User not found",
+            });
+        }
+    } catch (err) {
+        res.status(400).json({
+            status: 400,
+            message: err.message,
+        });
+    } 
+});
+
 // GET - Fetch list of body metric records of the user given the id
 router.get("/:userId/bmetric", async (req, res) => {
     try {
